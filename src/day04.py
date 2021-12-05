16b01a015a085733abb023cdea92cec9d21462b2
Daria2002 <dariamatkovicdaria@gmail.com>
2021-12-05 12:45:59 +0100

    Add day4, part2

--- src/day04.py
+++ src/day04.py
@@ -34,8 +34,32 @@ def resolve_matrix(matrix, nums):
                 return i, nums[i], matrix_sum
     return -1, -1, -1
 
-def part1(rows):
-    nums = [int(el) for el in rows[0].split(',')]
+def part1(nums, matrix_arr):
+    return winner_loser_result(matrix_arr, nums, True)
+
+def part2(nums, matrix_arr):
+    return winner_loser_result(matrix_arr, nums, False)
+
+def winner_loser_result(matrix_arr, nums, winner):
+    if winner:
+        tmp_i = len(nums) + 1
+    else:
+        tmp_i = -1
+    tmp_num = -1 # tmp is min or max dependig if we want loser or winner result
+    tmp_sum = -1
+    for matrix in matrix_arr:
+        i, num, matrix_sum = resolve_matrix(matrix, nums)
+        if winner and i < tmp_i:
+            tmp_i = i
+            tmp_num = num
+            tmp_sum = matrix_sum
+        elif not winner and i > tmp_i:
+            tmp_i = i
+            tmp_num = num
+            tmp_sum = matrix_sum
+    return tmp_num * tmp_sum
+
+def get_all_matrices(rows):
     matrix_arr = []
     tmp_matrix = []
     for i in range(2, len(rows)):
@@ -50,18 +74,11 @@ def part1(rows):
                 matrix_row.append(int(el))
         tmp_matrix.append(matrix_row)
     matrix_arr.append(tmp_matrix)
-
-    min_i = len(nums) + 1
-    min_num = -1
-    min_sum = -1
-    for matrix in matrix_arr:
-        i, num, matrix_sum = resolve_matrix(matrix, nums)
-        if i < min_i:
-            min_i = i
-            min_num = num
-            min_sum = matrix_sum
-    return min_num * min_sum
+    return matrix_arr
 
 if __name__ == "__main__":
     rows = get_all_input_rows('day04.txt')
-    print(f'part1 = {part1(rows)}')
+    matrixes = get_all_matrices(rows)
+    nums = [int(el) for el in rows[0].split(',')]
+    print(f'part1 = {part1(nums, matrixes)}')
+    print(f'part2 = {part2(nums, matrixes)}')
