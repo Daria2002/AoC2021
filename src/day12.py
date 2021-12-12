fdad2e0621aa455269467fe2166d2d573f5c594d
Daria2002 <dariamatkovicdaria@gmail.com>
2021-12-12 23:12:14 +0100

    Add day12, p2, too slow

--- src/day12.py
+++ src/day12.py
@@ -21,7 +21,7 @@ def part1(paths, curr_position, curr_path, visited):
         return 1
     count = 0
     for potential_path in paths[curr_position]:
-        if (potential_path not in paths and potential_path != "end") or (visited[potential_path] and potential_path.islower()):
+        if potential_path != "end" and ((potential_path not in paths) or (visited[potential_path] and potential_path.islower())):
             continue
         tmp_path = list(curr_path)
         tmp_path.append(potential_path)
@@ -46,7 +46,52 @@ def transform_input(rows):
         visited[start] = False if start != 'start' else True
     return data, visited
 
+def small_cave_check(paths):
+    count_dict = {}
+    visit_twice = False
+    for path in paths:
+        if path.isupper():
+            continue
+        if path not in count_dict:
+            count_dict[path] = 0
+        count_dict[path] += 1
+        if count_dict[path] > 1 and not visit_twice:
+            visit_twice = True
+        elif count_dict[path] > 1 and visit_twice:
+            return False
+    return True
+
+def part2(paths, curr_position, curr_path, visit_counter):
+    if curr_position == 'end':
+        if small_cave_check(curr_path):
+            return 1
+        return 0
+    count = 0
+    for potential_path in paths[curr_position]:
+        if potential_path == 'start' or (potential_path != "end" and ((potential_path not in paths) or (visit_counter[potential_path] > 2 and potential_path.islower()))):
+            continue
+        tmp_path = list(curr_path)
+        tmp_path.append(potential_path)
+        visit_counter[potential_path] += 1
+        count += part2(paths, potential_path, tmp_path, visit_counter)
+        visit_counter[potential_path] -= 1
+    return count
+
+def generate_visit_counter(rows):
+    visit_counter = {}
+    for row in rows:
+        start, end = row.split('-')
+        if start == 'start':
+            visit_counter[start] = 1
+        else:
+            visit_counter[start] = 0
+        if end != 'start':
+            visit_counter[end] = 0
+    return visit_counter
+
 if __name__ == '__main__':
     rows = get_all_input_rows('day12.txt')
     paths, visited = transform_input(rows)
     print(f'part 1: {part1(paths, "start", ["start"], visited)}')
+    visit_counter = generate_visit_counter(rows)
+    print(f'part 2: {part2(paths, "start", ["start"], visit_counter)}')
