1aa587621ab7b321de5a98dab73d933f49e482dc
Daria2002 <dariamatkovicdaria@gmail.com>
2021-12-14 18:48:49 +0100

    Add some code (not tested) for p2

--- src/day14.py
+++ src/day14.py
@@ -4,7 +4,6 @@ from helper.funcs import *
 
 def part1(polymer_template, pair_insertion_rules, steps):
     for step in range(0, steps):
-        print(f'Step {step}')
         result = ""
         for i in range(0, len(polymer_template) - 1, 1):
             if i == 0:
@@ -26,7 +25,37 @@ def part1(polymer_template, pair_insertion_rules, steps):
     return max_count - min_count
 
 def part2(polymer_template, pair_insertion_rules, steps):
-    return part1(polymer_template, pair_insertion_rules, steps)
+    pairs = []
+    count_elements = {}
+    for step in range(0, steps):
+        print(f'Step {step}')
+        count_elements = {}
+        if step == 0:
+            for i in range(0, len(polymer_template) - 1):
+                pairs.append(polymer_template[i:i+2])
+        new_pairs = []
+        for i, pair_ in enumerate(pairs):
+            insertion_el = pair_insertion_rules[pair_]
+            new_pairs.append(pair_[0] + insertion_el)
+            new_pairs.append(insertion_el + pair_[1])
+            if pair_[0] not in count_elements:
+                count_elements[pair_[0]] = 0
+            if pair_[1] not in count_elements:
+                count_elements[pair_[1]] = 0
+            if insertion_el not in count_elements:
+                count_elements[insertion_el] = 0
+            if i == 0:
+                count_elements[pair_[0]] += 1
+            count_elements[pair_[1]] += 1
+            count_elements[insertion_el] += 1
+        pairs = new_pairs
+    max_val, min_val = 0, sys.maxsize
+    for el, count_val in count_elements.items():
+        if count_val > max_val:
+            max_val = count_val
+        if count_val < min_val:
+            min_val = count_val
+    return max_val - min_val
 
 if __name__ == '__main__':
     rows = get_all_input_rows('day14.txt')
